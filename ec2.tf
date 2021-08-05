b229db9aaef2ba2fa86c202970c77b2a951ba6d0
musaevan <musaevan@gmail.com>
2021-08-05 00:33:17 +0000

    added ec2

---
+++ ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_key_pair" "key" {
+  key_name   = "key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c2b8ca1dad447f8a"
+  instance_type = "t3.micro"
+  count = 3
+  availability_zone = "us-east-1a"
+  associate_public_ip_address = true
+  monitoring = true
+  key_name = aws_key_pair.key.key_name
+  user_data  =  file("user_data.sh")
+  tags = {
+      Name   = "Wordpress"
+      Team   = "DevOps"
+      Environment = "Dev"
+  }
+}
---
+++ user_data.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+sudo yum install httpd
+sudo systemctl start httpd
