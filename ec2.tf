1ad0e069e00b57c8e4b89f1029ccc5e02349291a
musaevan <musaevan@gmail.com>
2021-08-05 02:19:22 +0000

    added route53

--- ec2.tf
+++ ec2.tf
@@ -13,3 +13,15 @@ resource "aws_instance" "web" {
       Environment = "Dev"
   }
 }
+
+resource "aws_route53_record" "blog" {
+  zone_id =  "Z04998252VNHGWXLLKP3E"
+  name    =  "blog.awsnazik.com"
+  type    =  "A"
+  ttl     =  "60"
+  records = [
+         aws_instance.web[0].public_ip, 
+         aws_instance.web[1].public_ip, 
+         aws_instance.web[2].public_ip, 
+         ]
+}
